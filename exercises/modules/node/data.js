28d4b64f6379dbd9b7f7bc41b10e4e146110e830
Gregor Schneider <gregor@rheingold.websales.united.domain>
2019-04-04 17:36:20 +0200

    first commit

---
+++ exercises/modules/node/data.js
@@ -0,0 +1,10 @@
+window.App = {
+  users: [
+    {id: 1, name: 'Weezy'}
+  ],
+  posts: [
+    {title: 'yo', body: 'I ate today', createdBy: 1},
+    {title: 'Me', body: 'Look at my selfie', createdBy: 1},
+    {title: 'My doggy', body: 'my dog is better than yours', createdBy: 1}
+  ]
+}
