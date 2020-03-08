3c6a764a8622434b554707d7d769c3fb055d7c2a
zacharyarney <zachary.arney@gmail.com>
2020-03-07 17:38:11 -0800

    browser to node conversion

---
+++ exercises/modules/node/app.js
@@ -0,0 +1,28 @@
+const { getUserById, getPostsForUser } = require('./api');
+
+const showPostsForCurrentUser = (userId, cb) => {
+  getPostsForUser(userId, (posts) => {
+    const postTemplates = posts.map((post) => {
+      return `
+      <div class="post">
+        ${post.title}
+        ${post.body}
+        ${post.createdBy}
+      </div>`;
+    });
+    cb(postTemplates);
+  });
+};
+
+const showUserProfile = (userId, cb) => {
+  getUserById(userId, (user) => {
+    const profile = `
+      <div>
+        ${user.name}
+      </div>
+    `;
+    cb(user);
+  });
+};
+
+module.exports = { showPostsForCurrentUser, showUserProfile };
