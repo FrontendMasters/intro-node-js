27c9ec40f8ea981431f1473de98b12c37837f84f
Matthew Bolton <matthewwbolton@gmail.com>
2020-12-15 13:30:38 -0500

    commit changes

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -1,8 +1,8 @@
-const http = require('http')
-const url = require('url')
-const fs = require('fs')
-const path = require('path')
-const mime = require('mime')
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const mime = require("mime");
 
 /**
  * async function that reads asset from disk
@@ -10,56 +10,57 @@ const mime = require('mime')
  */
 const findAsset = (name) => {
   return new Promise((resolve, reject) => {
-    const assetPath = path.join(__dirname, 'assets', name)
-    fs.readFile(assetPath, {encoding: 'utf-8'}, (err, asset) => {
+    const assetPath = path.join(__dirname, "assets", name);
+    fs.readFile(assetPath, { encoding: "utf-8" }, (err, asset) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(asset)
+        resolve(asset);
       }
-    })
-  })
-}
+    });
+  });
+};
 
-const hostname = '127.0.0.1'
-const port = 3000
+const hostname = "127.0.0.1";
+const port = 3000;
 // simple, quick router object
 const router = {
-  '/ GET': {
-    asset: 'index.html',
-    type: mime.getType('html')
+  "/ GET": {
+    asset: "index.html",
+    type: mime.getType("html"),
   },
-  '/style.css GET': {
-    asset: 'style.css',
-    type: mime.getType('css')
-  }
-}
+  "/style.css GET": {
+    asset: "style.css",
+    type: mime.getType("css"),
+  },
+};
 
 // log incoming request coming into the server. Helpful for debugging and tracking
-const logRequest = (method, route, status) => console.log(method, route, status)
+const logRequest = (method, route, status) =>
+  console.log(method, route, status);
 
 const server = http.createServer(async (req, res) => {
-  const method = req.method
-  const route = url.parse(req.url).pathname
-  // check the router for the incomming route + method pair
-  const routeMatch = router[`${route} ${method}`]
+  const method = req.method;
+  const route = url.parse(req.url).pathname;
+  // check the router for the incoming route + method pair
+  const routeMatch = router[`${route} ${method}`];
   // return not found if the router does not have a match
   if (!routeMatch) {
-    res.writeHead(404)
-    logRequest(method, route, 404)
-    return res.end()
+    res.writeHead(404);
+    logRequest(method, route, 404);
+    return res.end();
   }
 
-  const {type, asset} = routeMatch
+  const { type, asset } = routeMatch;
 
   // set the content-type header for the asset so applications like a browser will know how to handle it
-  res.writeHead(200,{'Content-Type': type})
+  res.writeHead(200, { "Content-Type": type });
   // most important part, send down the asset
-  res.write(await findAsset(asset))
-  logRequest(method, route, 200)
-  res.end()
-})
+  res.write(await findAsset(asset));
+  logRequest(method, route, 200);
+  res.end();
+});
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
