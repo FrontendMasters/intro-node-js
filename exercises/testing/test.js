cfb566833348a3dacba0c8596ca7c8cc50d7c283
Matthew Bolton <matthewwbolton@gmail.com>
2020-12-18 11:43:13 -0500

    fixed all bugs in api.js and wrote unit test for fixId function in users.js

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,7 @@
+const { fixId } = require("./users");
 // write some tests
-describe('users', () => {
-  
-})
+describe("users", () => {
+  test("fixId", () => {
+    expect(fixId("200")).toBe(200);
+  });
+});
