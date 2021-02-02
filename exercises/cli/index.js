b9154139e8a109227fa471d3fceb2473bd9737a9
abdelfatah <abdelfatahhezema@gmail.com>
2021-02-02 16:22:38 +0200

    server excercise

--- exercises/cli/index.js
+++ exercises/cli/index.js
@@ -1,45 +1,41 @@
 // install any missing modules
-const program = require('commander')
-const { prompt } = require('inquirer')
-const {newContactPrompts} = require('./prompts')
-const {getContacts, saveContacts} = require('./utils')
+const program = require("commander");
+const { prompt } = require("inquirer");
+const { newContactPrompts } = require("./prompts");
+const { getContacts, saveContacts } = require("./utils");
+
+program.version("0.0.1").description("Address book CLI program");
 
 program
-  .version('0.0.1')
-  .description('Address book CLI program')
-
-program
-  .command('new')
-  .alias('n')
-  .description('add a new contact')
+  .command("new")
+  .alias("n")
+  .description("add a new contact")
   .action(() => {
-    prompt(newContactPrompts)
-      .then(({firstName, lastName, phoneNumber}) => {
-        const key = firstName + ' ' + lastName
-        const contacts = getContacts()
-        contacts[key] = {firstName, lastName, phoneNumber}
-        saveContacts(contacts)
-      })
-  })
+    prompt(newContactPrompts).then(({ firstName, lastName, phoneNumber }) => {
+      const key = firstName + " " + lastName;
+      const contacts = getContacts();
+      contacts[key] = { firstName, lastName, phoneNumber };
+      saveContacts(contacts);
+    });
+  });
 
 program
-  .command('list')
-  .alias('l')
-  .description('list all contacts')
+  .command("list")
+  .alias("l")
+  .description("list all contacts")
   .action(() => {
-    const contacts = getContacts()
+    const contacts = getContacts();
     prompt([
       {
-        type: 'list',
-        name: 'selected',
-        message: 'Select a contact',
-        choices: Object.keys(contacts)
-      }
-    ])
-      .then(({selected}) => {
-        const contact = contacts[selected]
-        console.log(JSON.stringify(contact, null, 2))
-      })
-  })
+        type: "list",
+        name: "selected",
+        message: "Select a contact",
+        choices: Object.keys(contacts),
+      },
+    ]).then(({ selected }) => {
+      const contact = contacts[selected];
+      console.log(JSON.stringify(contact, null, 2));
+    });
+  });
 
-program.parse(process.argv)
+program.parse(process.argv);
--- exercises/cli/utils.js
+++ exercises/cli/utils.js
@@ -1,8 +1,9 @@
-const fs = require('fs')
-const path = require('path')
+const { json } = require("body-parser");
+const fs = require("fs");
+const path = require("path");
 
 // this path needs to be relative to work with fs
-const contactsLocation = 'contacts.json'
+const contactsLocation = path.join(__dirname, "./contacts.json");
 
 /**
  * should read the contacts at the
@@ -10,8 +11,9 @@ const contactsLocation = 'contacts.json'
  * it to a js object
  */
 const getContacts = () => {
-  
-}
+  const contacts = fs.readFileSync(contactsLocation).toString();
+  return JSON.parse(contacts);
+};
 
 /**
  * takes a contacts object, converts it to JSON
@@ -19,12 +21,11 @@ const getContacts = () => {
  * @param {Object} contacts contacts object
  */
 const saveContacts = (contacts) => {
-
-}
+  fs.writeFileSync(contactsLocation, JSON.stringify(contacts, null, 2));
+};
 
 module.exports = {
   contactsLocation,
   getContacts,
-  saveContacts
-}
-
+  saveContacts,
+};
